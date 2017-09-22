50d8fa935d86cb1c72e72f9b0b4f9f7db8e02818
Toshifumi Katayama <t-katayama.sra@gmo-click.com>
2017-09-22 12:11:51 +0900

    MainActivity の名前を RepositoryListActivity に変更

--- KotlinMVVMExample/app/src/main/java/com/katatoshi/kotlinmvvmexample/RepositoryListActivity.kt
+++ KotlinMVVMExample/app/src/main/java/com/katatoshi/kotlinmvvmexample/RepositoryListActivity.kt
@@ -3,10 +3,10 @@ package com.katatoshi.kotlinmvvmexample
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RepositoryListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_repository_list)
     }
 }
